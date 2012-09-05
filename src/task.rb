70c1cd1e4c66568bc4b93953ffa43e7a9552e117
Honda Hiroki <hiro_4455@hotmail.com>
2012-09-05 23:08:54 +0900

    add module "Schedule"

--- src/task.rb
+++ src/task.rb
@@ -1,3 +1,28 @@
+module Schedule
+  class Calendar
+    @@defaults = Calendar.new
+	def self.defaults
+      @@defaults
+    end
+  end
+
+  attr_accessor :default_calendar
+
+  class Resource
+    def initialize(args={})
+      @id = args[:id]
+      @name = args[:name]
+      @calendar = Calendar::defaults
+    end
+    def duration(task)
+    end
+  end
+
+  p Resource.new({:name=>'abc'})
+
+
+
+
 class Task
   def initialize
     @dulation = 0
@@ -6,4 +31,9 @@ class Task
     @nexts = []
     @resource = []
   end
+end
+
+
+
+
 end
