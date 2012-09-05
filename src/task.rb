7d42fb3c3be668f8e3daa6042c74b06af80d7652
Honda Hiroki <honda@success-corp.co.jp>
2012-09-05 10:48:18 +0900

    Add to calculate duration with subtasks

--- src/task.rb
+++ src/task.rb
@@ -1,9 +1,30 @@
 class Task
-  def initialize
-    @dulation = 0
+  def initialize(duration)
+    @duration = duration
     @subtasks = []
     @previouses = []
     @nexts = []
     @resource = []
   end
+
+  def durations
+    subtask_duration = @subtasks.inject(0){ |res,subtask| res += subtask.durations }
+    if @duration > subtask_duration
+      return @duration
+    end
+    subtask_duration
+  end
+
+  def subtask(subtask)
+    if subtask.kind_of?(Enumerable)
+      @subtasks += subtask
+    end
+    self
+  end
 end
+
+def task(duration)
+  Task.new(duration)
+end
+
+puts task(5).subtask([task(1),task(2)]).durations
